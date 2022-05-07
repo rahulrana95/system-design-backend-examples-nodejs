f099a55056a4cabf7669e2fed5dc2ad695a9786f
Rahul Rana <3rahul4@gmail.com>
2022-05-07 20:56:34 +0530

    Remove all files

--- src/interfaces/vendors/INext.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Defines Custom method types over Express's NextFunction
- *
- * @author Faiz A. Farooqui <faiz@geeekyants.com>
- */
-
-import { NextFunction } from 'express';
-
-export interface INext extends NextFunction {}
